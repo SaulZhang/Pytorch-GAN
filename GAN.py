1667634f381d5abd3f54f074b374876f426ad1ff
SaulZhang <316225519@qq.com>
2019-05-02 00:58:12 +0800

    Update and rename GANpy to GAN.py

---
+++ GAN.py
@@ -0,0 +1,87 @@
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, sampler
+from torchvision.datasets import MNIST
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import torchvision.transforms as tfs
+
+NUM_TRAIN = 50000
+NUM_VAL = 5000
+
+NOISE_DIM = 96
+batch_size = 128
+
+
+class ChunkSampler(sampler.Sampler): # 定义一个取样的函数
+    """Samples elements sequentially from some offset. 
+    Arguments:
+        num_samples: # of desired datapoints
+        start: offset where we should start selecting from
+    """
+    def __init__(self, num_samples, start=0):
+        self.num_samples = num_samples
+        self.start = start
+
+    def __iter__(self):
+        return iter(range(self.start, self.start + self.num_samples))
+
+    def __len__(self):
+        return self.num_samples
+
+def show_images(images): # 定义画图工具
+    images = np.reshape(images, [images.shape[0], -1])
+    sqrtn = int(np.ceil(np.sqrt(images.shape[0])))
+    sqrtimg = int(np.ceil(np.sqrt(images.shape[1])))
+
+    fig = plt.figure(figsize=(sqrtn, sqrtn))
+    gs = gridspec.GridSpec(sqrtn, sqrtn)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for i, img in enumerate(images):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(img.reshape([sqrtimg,sqrtimg]))
+    return 
+
+
+def preprocess_img(x):
+    x = tfs.ToTensor()(x)
+    return (x - 0.5) / 0.5
+
+def deprocess_img(x):
+    return (x + 1.0) / 2.0
+
+def discriminator():
+    net = nn.Sequential(        
+            nn.Linear(784, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1)
+        )
+    return net
+
+def generator(noise_dim=NOISE_DIM):   
+    net = nn.Sequential(
+        nn.Linear(noise_dim, 1024),
+        nn.ReLU(True),
+        nn.Linear(1024, 1024),
+        nn.ReLU(True),
+        nn.Linear(1024, 784),
+        nn.Tanh()
+    )
+    return net
+
+bce_loss = nn.BCEWithLogitsLoss()
+
+def discriminator_loss(logits_real, logits_fake): # 判别器的 loss
+    size = logits_real.shape[0]
+    true_labels = Variable(torch.ones(size, 1)).float()
+    false_labels = Variable(torch.ze
